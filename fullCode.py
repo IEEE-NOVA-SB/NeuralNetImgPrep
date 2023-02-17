f18ee1ca378c8719a9700e6b1c4942e94261f1af
Tiago monteiro <tiago.monteiro0715@gmail.com>
2023-02-17 22:47:14 +0000

    Add files via upload

--- fullCode.py
+++ fullCode.py
@@ -1,22 +1,27 @@
+# Importing the libraries that will be used in the code.
 import os
 import rembg
 from PIL import Image
 
-# Set the input and output directories
+# Defining the input and output directories.
 input_dir = '/home/drifter/projects/remove-background/editar'
 output_dir = '/home/drifter/projects/remove-background/completo'
 
-# Iterate over all files in the input directory
+# Iterating over all the files in the input directory, and if the file is a jpg, png or jpeg, it opens
+# the file, removes the background, and saves the file in the output directory.
 for file_name in os.listdir(input_dir):
     if file_name.endswith('.jpg') or file_name.endswith('.png') or file_name.endswith('.jpeg'):
-        # Open the image and remove the background using Rembg
+        
+        # Opening the file, removing the background, and saving the file in the output directory.
         with open(os.path.join(input_dir, file_name), 'rb') as f:
             image = Image.open(f)
 
             output = rembg.remove(image)
             output = output.convert('RGB')#Porque não consegue converter imagens .jpeg. pois imagens .jpeg têm um canal alpha
 
-        # Save the output image to the output directory
+        # Joining the output directory and the file name.
         output_file = os.path.join(output_dir, file_name)
+
+        # Saving the file in the output directory.
         with open(output_file, 'wb') as f:
             output.save(f)
